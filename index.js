488adef789738e6b5d64de81760b2cdfd879940b
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2022-11-15 17:56:48 +0100

    add options

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const inputValue = document.querySelector(".inputValue");
-const currency = document.querySelector(".currency");
+const currency = document.querySelector("#currency");
 const countsButton = document.querySelector(".countsButton");
 const result = document.querySelector(".result");
+const url = "http://api.nbp.pl/api/exchangerates/rates/a/";
+
+const currencyTable = ["EUR", "USD", "CHF"];
+
+currencyTable.forEach((element) => {
+  let opt = document.createElement("option");
+  console.log(element);
+  opt.value = element;
+  opt.innerHTML = element;
+  currency.appendChild(opt);
+});
