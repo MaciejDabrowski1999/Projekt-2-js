89d223ba3b6598412825c04586898c3e85fdf120
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2022-11-15 16:29:46 +0100

    add constanses

---
+++ index.js
@@ -0,0 +1,4 @@
+const inputValue = document.querySelector(".inputValue");
+const currency = document.querySelector(".currency");
+const countsButton = document.querySelector(".countsButton");
+const result = document.querySelector(".result");
