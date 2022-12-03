38c60811ac124de757f8746dc4406582a357413d
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2022-12-03 14:10:28 +0100

    import api

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const inputValue = document.querySelector(".inputValue");
+const inputValue = document.querySelector("#inputValue");
 const currency = document.querySelector("#currency");
-const countsButton = document.querySelector(".countsButton");
-const result = document.querySelector(".result");
+const countsButton = document.querySelector("#countsButton");
+const result = document.querySelector("#result");
 const url = "http://api.nbp.pl/api/exchangerates/rates/a/";
 
 const currencyTable = ["EUR", "USD", "CHF"];
 
 currencyTable.forEach((element) => {
   let opt = document.createElement("option");
-  console.log(element);
   opt.value = element;
   opt.innerHTML = element;
   currency.appendChild(opt);
+});
+
+const value = (rates) => {
+  const valueCurrency = rates[0].mid;
+  const sum = valueCurrency * inputValue.value;
+  result.innerHTML = sum;
+  console.log(sum);
+};
+
+countsButton.addEventListener("click", () => {
+  let valueOption = currency.value;
+  fetch(url + valueOption)
+    .then((response) => response.json())
+    .then((data) => value(data.rates))
+    .catch((err) => console.error(err));
 });
