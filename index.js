f04c4a1f024d43aa765fa3ee752cbfb5f0ce3717
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2022-12-03 15:42:30 +0100

    add style

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const inputValue = document.querySelector("#inputValue");
 const currency = document.querySelector("#currency");
 const countsButton = document.querySelector("#countsButton");
 const result = document.querySelector("#result");
+const eurBtn = document.querySelector("#eur");
+const usdBtn = document.querySelector("#usd");
+const chfBtn = document.querySelector("#chf");
+
 const url = "http://api.nbp.pl/api/exchangerates/rates/a/";
 
 const currencyTable = ["EUR", "USD", "CHF"];
 
 currencyTable.forEach((element) => {
   let opt = document.createElement("option");
+  opt.setAttribute("class", "inputStyle");
   opt.value = element;
   opt.innerHTML = element;
   currency.appendChild(opt);
@@ -17,7 +22,6 @@ const value = (rates) => {
   const valueCurrency = rates[0].mid;
   const sum = valueCurrency * inputValue.value;
   result.innerHTML = sum;
-  console.log(sum);
 };
 
 countsButton.addEventListener("click", () => {
@@ -26,4 +30,13 @@ countsButton.addEventListener("click", () => {
     .then((response) => response.json())
     .then((data) => value(data.rates))
     .catch((err) => console.error(err));
+});
+eurBtn.addEventListener("click", () => {
+  currency.value = currencyTable[0];
+});
+usdBtn.addEventListener("click", () => {
+  currency.value = currencyTable[1];
+});
+chfBtn.addEventListener("click", () => {
+  currency.value = currencyTable[2];
 });
